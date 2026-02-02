17b9eaf7d2fcd8ea46de80378f1e3a6a36e44007
Yash Tiwari <tiwariyash515@gmail.com>
2026-02-02 22:33:12 +0530

    Backend Updation

--- backend/inngest/index.js
+++ backend/inngest/index.js
@@ -51,6 +51,75 @@ export const syncUserUpdate = inngest.createFunction(
   } 
 );
 
+export const syncWorkspaceCreation = inngest.createFunction(
+  { id: "sync-workspace-from-clerk" },
+  { event: "clerk/organization.created" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.create({
+      data: {
+        id: data.id,
+        name: data.name,
+        slug: data.slug,
+        ownerId: data.created_by,
+        image_url: data.image_url || "",
+      },
+    });
+    await prisma.workspaceMember.create({
+      data: {
+        userId: data.created_by,
+        workspaceId: data.id,
+        role: "ADMIN",
+      },
+    });
+  }
+);
+
+export const syncWorkspaceDeletion = inngest.createFunction(
+  { id: "delete-workspace-from-clerk" },
+  { event: "clerk/organization.deleted" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.deleteMany({
+      where: {
+        id: data.id,
+      },
+    });
+  } 
+);
+
+export const syncWorkspaceUpdate = inngest.createFunction(
+  { id: "update-workspace-from-clerk" },
+  { event: "clerk/organization.updated" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspace.updateMany({
+      where: {
+        id: data.id,
+      },
+      data: {
+        name: data.name,
+        slug: data.slug,
+        image_url: data.image_url || "",
+      },
+    });
+  }
+);
+
+export const syncWorkspaceMemberCreation = inngest.createFunction(
+  { id: "sync-workspace-member-from-clerk" },
+  { event: "clerk/organizationInvitation.created" },
+  async ({ event }) => {
+    const { data } = event;
+    await prisma.workspaceMember.create({
+      data: {
+        userId: data.user_id,
+        workspaceId: data.organization_id,
+        role: String(data.role_name).toUpperCase(),
+      },
+    });
+  }
+);
 
 // IMPORTANT — export function here
-export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdate];
+export const functions = [syncUserCreation, syncUserDeletion, syncUserUpdate, syncWorkspaceCreation, syncWorkspaceDeletion, syncWorkspaceUpdate, syncWorkspaceMemberCreation ];
